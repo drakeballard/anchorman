870f4d135560da3129281ff7b699e0ab0224bfb1
manalimatkar <manali.matkar@gmail.com>
2016-11-29 17:03:46 -0600

    added sourceList and searchSource Handlebars and path for source search

--- controllers/application_controller.js
+++ controllers/application_controller.js
@@ -8,28 +8,53 @@ res.render('home');
 });
 
 router.get('/news/:category', function(req,res) {
+
 	var searchCategory = req.params.category;
 		if (searchCategory == "all"){
         searchCategory = "";
     }
 
-news.getNews(searchCategory, function(data){
-  // console.log(data);
-  res.render('articles/articles', {articlesbysource: data});
-});
+	news.getNews(searchCategory, function(data){
+	  // console.log(data);
+	  res.render('articles/articles', {articlesbysource: data});
+	});
 
 });
 
 router.get('/news/:source/:sortBy', function(req,res){
+
 	var bySource = req.params.source;
 	var sortBy = req.params.sortBy;
 
 	console.log("====================INSIDE GETNEWSBYSOURCE ========================="+bySource + sortBy);
 
 	news.getNewsBySource(bySource, sortBy, function(result){
-	  console.log(result);
+	  // console.log(result);
 	  res.render('articles/articlesBySource', {articlesList: result});
 	});
+
+});
+
+router.get('news/:category/:language/:country', function(req,res){
+	var category = req.params.category;
+	var language = req.params.language;
+	var country = req.params.country;
+	if (category=="all") {
+		category = "";	
+	}
+	if (language=="all") {
+		language = "";	
+	}
+	if (country=="all") {
+		country = "";	
+	}
+
+	news.getSourceList(category, language, country, function(result){
+		console.log(res);
+		res.render('sources/sourceList', {sourceList: result});
+	});
+
+
 
 });
 
